0352678af803ca8115f207eca409f4b61d7bddfc
hyper-neutrino <alex.yj.liao@gmail.com>
2024-03-16 12:19:16 -0400

    fix migrator

--- packages/backend/src/db/migrate.ts
+++ packages/backend/src/db/migrate.ts
@@ -1,4 +1,5 @@
-import { migrate } from "drizzle-orm/planetscale-serverless/migrator";
+import { migrate } from "drizzle-orm/mysql2/migrator";
 import { db } from "./db.js";
 
 await migrate(db, { migrationsFolder: "./drizzle" });
+process.exit(0);
